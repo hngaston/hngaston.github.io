01705dbd678d7ad43cfa2aa68bb0f7928cadb630
hngaston <63380433+hngaston@users.noreply.github.com>
2025-08-06 05:18:49 +0000

    Deploying to gh-pages from @ hngaston/hngaston.github.io@f82462292792cd4ca6c89020e3f6396c653833c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-resume",
-          title: "resume",
-          description: "Current Resume (updated 8/25)",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/resume/";
-          },
-        },{id: "books-the-godfather",
+  },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
